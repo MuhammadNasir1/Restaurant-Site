55d28c5618795287acdea06e1282c208b466e5a2
Nasir <nmian7080@gmail.com>
2022-12-31 14:42:50 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// .....Navbar scroll.....
+var nav=document.querySelector('nav');
+window.addEventListener('scroll', function(){
+  if(window.pageYOffset>100){
+    nav.classList.add('bg-dark', 'shadow');
+  }else{nav.classList.remove('bg-dark', 'shadow')}
+})
+
+// .....Modal-Searchbar.....
+// const myModal = document.getElementById('myModal')
+// const myInput = document.getElementById('myInput')
+
+// myModal.addEventListener('shown.bs.modal', () => {
+//   myInput.focus()
+// })
+
+
+
+
