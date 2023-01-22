30d8c10e264395b715d3c33a1e9762c9d9ce1281
Nasir <nmian7080@gmail.com>
2023-01-22 14:57:04 +0500

    final project

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// .....Navbar scroll.....
+//   .....Navbar scroll.....
 var nav=document.querySelector('nav');
 window.addEventListener('scroll', function(){
   if(window.pageYOffset>100){
@@ -6,9 +6,17 @@ window.addEventListener('scroll', function(){
   }else{nav.classList.remove('bg-dark', 'shadow')}
 })
 
+// ......login...... 
+  // const signUpButton = document.getElementById('signUp');
+  // const signInButton = document.getElementById('signIn');
+  // const container = document.getElementById('container');
 
+  // signUpButton.addEventListener('click', () => {
+  //   container.classList.add("right-panel-active");
+  // });
 
-
-
+  // signInButton.addEventListener('click', () => {
+  //   container.classList.remove("right-panel-active");
+  // });
 
 
